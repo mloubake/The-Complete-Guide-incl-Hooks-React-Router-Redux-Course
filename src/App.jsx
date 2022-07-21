6569cb9a2e398d06673d729dbef6617a5c9836c9
Matheus Loubake <mloubake@hotmail.com>
2022-07-21 14:46:19 -0300

    Creating and displaying ExpenseItem Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Expense Tracker</h1>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
