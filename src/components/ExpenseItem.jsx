5d21707cd5664724b021b0149fc182eb10f6e57f
Matheus Loubake <mloubake@hotmail.com>
2022-07-21 16:12:42 -0300

    Creating CSS for Expense Item Component.

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div className="expense-item">
+      <h2>July 21th 2022</h2>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294,67</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
