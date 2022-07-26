e7aab9f7ed9180cb4fbc242f5f9e5358c96a7d15
Matheus Loubake <mloubake@hotmail.com>
2022-07-26 14:51:16 -0300

    Continuing from Section 3.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseList />
+    </div>
+  );
+}
+
+export default App;
