bc14b9826ea7020317a068d54d8575c72802d75a
Matheus Loubake <mloubake@hotmail.com>
2022-08-09 19:23:35 -0300

    Moving <ErrorModal> to outside <body>, using ReactDom.CreatePortal.

--- src/components/ErrorModal/ErrorModal.jsx
+++ src/components/ErrorModal/ErrorModal.jsx
@@ -1,25 +1,48 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
 import Button from "../Button/Button";
 
 import classes from "./styles.module.css";
 
-function ErrorModal({ error, onCloseModal }) {
+const Backdrop = ({ children, onCloseModal }) => {
   return (
     <div className={classes.backdrop} onClick={onCloseModal}>
-      <div className={classes.modal}>
-        <header>
-          <label>{error.title}</label>
-        </header>
-
-        <main>
-          <label>{error.message}</label>
-        </main>
-
-        <footer>
-          <Button text={"Close"} onClick={onCloseModal} />
-        </footer>
-      </div>
+      {children}
     </div>
+  );
+};
+
+const ModalOverlay = ({ error, onCloseModal }) => {
+  return (
+    <div className={classes.modal}>
+      <header>
+        <label>{error.title}</label>
+      </header>
+
+      <main>
+        <label>{error.message}</label>
+      </main>
+
+      <footer>
+        <Button text={"Close"} onClick={onCloseModal} />
+      </footer>
+    </div>
+  );
+};
+
+function ErrorModal({ error, onCloseModal }) {
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onCloseModal={onCloseModal} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay error={error} onCloseModal={onCloseModal} />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 }
 
